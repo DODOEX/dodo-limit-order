15a3426852d57992020a92e86540c1e98a890afd
owen05 <liuhongnan53@163.com>
2021-11-24 09:19:51 +0800

    rfq test

--- config/rinkeby-config.js
+++ config/rinkeby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
     RINKEBY_CONFIG: {
-        DODOLimitOrder: "",
-        DODOLimitOrderBot: "",
+        DODOLimitOrder: "0x2093bC118E6B4aBFB0B3f61E2d01e9100c93B713",
+        DODOLimitOrderBot: "0xFff90AA817D06801d8B8A9B8B0E295C7708AeD30",
         //Approve
         DODOApprove: "0xcC8d87A7C747eeE4242045C47Ef25e0A81D56ae3",
         DODOApproveProxy: "0x790917CA55c9B01974BFDd066537Fb3DF42Bb0E3",
--- script/fillOrder.js
+++ script/fillOrder.js
@@ -24,11 +24,11 @@ async function makeData() {
     var maker = "0xbC7814de9e42945C9fFd89D2BFff1a45e07Bdb10";
     var taker = dodoLimitOrderBotAddress;
     var dodoProxy = "0xba001E96AF87bF9d8D0BDA667067A9921FE6d294";
-    var dodoApiData = "0xf87dc1b7000000000000000000000000ce7afae63edd2719cd9f46f8afa80eb490ede6a80000000000000000000000000f423838d2e4ce4314d30f8ad42dc41cecde06c50000000000000000000000000000000000000000000000000de0b6b3a7640000000000000000000000000000000000000000000000000000837d0d63fe6d1f1400000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000619d0e330000000000000000000000000000000000000000000000000000000000000001000000000000000000000000ba76f249c40101427ec9d9823c251172f3e53548";
-    var signature = "0xdcc5a13ed9a74198f10c8cf6c0c548f7cd0d29160f97c5f91dff7d4f80a973df799918a63462342ea922ad9dadcb006a5d43a684d4a11db727487319ac69cf251b";
-    var expiration = 1638544313;
-    var saltOrSolt = 1283598123731;
-    
+    var dodoApiData = "0xf87dc1b70000000000000000000000000f423838d2e4ce4314d30f8ad42dc41cecde06c5000000000000000000000000ce7afae63edd2719cd9f46f8afa80eb490ede6a80000000000000000000000000000000000000000000000008ac7230489e800000000000000000000000000000000000000000000000000000d376626f626f19d00000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000619d97ae0000000000000000000000000000000000000000000000000000000000000001000000000000000000000000ba76f249c40101427ec9d9823c251172f3e53548";
+    var signature = "0xeb4079eb2c728f0907f4c567bcf5f587abfdc276df5988d1b64862222e33b0be7e820890888ac3687cddd06cb32dab5353010733bb22247609c7d16ccf60dd621b";
+    var expiration = 1638579989;
+    var saltOrSolt = 363880468936;
+
     //step one: 构造doLimitOrderSwap
     var dodoLimitOrderSwapData = dodoLimitOrderBotInstance.methods.doLimitOrderSwap(
         0,
@@ -40,16 +40,16 @@ async function makeData() {
     ).encodeABI();
 
     //step two: 构造DODOLimitOrder的 fillLimitOrder
-    var limitOrder = {
-        makerToken: makerToken,
-        takerToken: takerToken,
-        makerAmount: parseFixed(makerAmount, 18).toString(),
-        takerAmount: parseFixed(takerAmount, 18).toString(),
-        maker: maker,
-        taker: taker,
-        expiration: expiration,
-        saltOrSolt: saltOrSolt
-    }
+    var limitOrder = [
+        makerToken,
+        takerToken,
+        parseFixed(makerAmount, 18).toString(),
+        parseFixed(takerAmount, 18).toString(),
+        maker,
+        taker,
+        expiration,
+        saltOrSolt
+    ]
 
     var fillLimitOrderData = dodoLimitOrderInstance.methods.fillLimitOrder(
         limitOrder,
@@ -59,6 +59,7 @@ async function makeData() {
         dodoLimitOrderSwapData
     ).encodeABI();
 
+    console.log("fillLimitOrderData:", fillLimitOrderData);
 
     //step three: 构造最上层DODOLimitOrderBot
     var data = dodoLimitOrderBotInstance.methods.fillDODOLimitOrder(
@@ -77,36 +78,34 @@ async function makeRFQData() {
     var makerAmount = "1";
     var takerAmount = "10";
     var maker = "0x7e83d9d94837eE82F0cc18a691da6f42F03F1d86";
-    var taker = "0xbC7814de9e42945C9fFd89D2BFff1a45e07Bdb10";
-    var signature = "0x01faed0170a4131734ce86561da4460702d0e07b55404b7eb111c45253f9c58e7474a40a4999848c868dc39650b639660bda7905e151078dd84419efa97a875b1b";
-    var expiration = "1638543769";
-    var saltOrSolt = "1572582376302";
+    var taker = "0x0000000000000000000000000000000000000000";
+    var signature = "0x48714508bef5cc8ba80723937900e7a584327fbc4ed61076d82571d4442c39d05c53541d7b33fedafb7ab7dcbbd2460e1d8d619b880fb719aa33f5826022eb521c";
+    var expiration = 1638579123;
+    var saltOrSolt = 1499822599887;
 
-    var rfqOrder = {
-        makerToken: makerToken,
-        takerToken: takerToken,
-        makerAmount: parseFixed(makerAmount, 18).toString(),
-        takerAmount: parseFixed(takerAmount, 18).toString(),
-        maker: maker,
-        taker: taker,
-        expiration: expiration,
-        saltOrSolt: saltOrSolt
-    };
+    var rfqOrder = [
+        makerToken,
+        takerToken,
+        parseFixed(makerAmount, 18).toString(),
+        parseFixed(takerAmount, 18).toString(),
+        maker,
+        taker,
+        expiration,
+        saltOrSolt
+    ]
 
     console.log("rfqOrder:", rfqOrder)
 
-    try {
-        var data = dodoLimitOrderInstance.methods.fillRFQ(
-            rfqOrder,
-            signature,
-            parseFixed(takerAmount, 18).toString(),//all filled
-            "0",
-            "0x0000000000000000000000000000000000000000"
-        ).encodeABI();
+    var data = dodoLimitOrderInstance.methods.fillRFQ(
+        rfqOrder,
+        signature,
+        parseFixed(takerAmount, 18).toString(),//all filled
+        "0",
+        "0xbC7814de9e42945C9fFd89D2BFff1a45e07Bdb10"
+    ).encodeABI();
 
-        console.log("data:", data)
-    } catch(e) {
-        console.log("e:",e)
-    }
+    console.log("data:", data)
 
 }
+
+
