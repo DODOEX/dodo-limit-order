41d34d2596f4a4a75a50d0ee330f6f739ae1212c
owen05 <liuhongnan53@163.com>
2022-03-29 00:48:06 +0800

    add rfqBot

---
+++ contracts/DODORfqOrderBot.sol
@@ -0,0 +1,181 @@
+/*
+    Copyright 2021 DODO ZOO.
+    SPDX-License-Identifier: Apache-2.0
+*/
+
+pragma solidity 0.8.4;
+
+import {InitializableOwnable} from "./lib/InitializableOwnable.sol";
+import {IDODOApproveProxy} from "./intf/IDODOApproveProxy.sol";
+import {IERC20} from "./intf/IERC20.sol";
+import {SafeMath} from "./lib/SafeMath.sol";
+import {SafeERC20} from "./lib/SafeERC20.sol";
+import {EIP712} from "./external/draft-EIP712.sol";
+import {ECDSA} from "./external/ECDSA.sol";
+
+/**
+ * @title DODORfqOrderBot
+ * @author DODO Breeder
+ */
+
+ contract DODORfqOrderBot is EIP712("DODO Limit Order Protocol", "1"), InitializableOwnable {
+    using SafeMath for uint256;
+    using SafeERC20 for IERC20;
+
+    //=============== Storage ===============
+    address public _TOKEN_RECEIVER_;
+    address public _DODO_APPROVE_;
+    address public _DODO_APPROVE_PROXY_;
+    mapping (address => bool) public isAdminListed;
+
+    bytes32 constant public RFQ_ORDER_TYPEHASH = keccak256(
+        "Order(address makerToken,address takerToken,uint256 makerAmount,uint256 takerAmount,uint256 makerTokenFeeAmount,uint256 takerFillAmount,address maker,address taker,uint256 expiration,uint256 slot)"
+    );
+
+    struct RfqOrder {
+        address makerToken;
+        address takerToken;
+        uint256 makerAmount;
+        uint256 takerAmount;
+        uint256 makerTokenFeeAmount;
+        uint256 takerFillAmount;
+        address maker;
+        address taker;
+        uint256 expiration;
+        uint256 slot;
+    }
+    
+    //=============== Event ===============
+    event addAdmin(address admin);
+    event removeAdmin(address admin);
+    event changeReceiver(address newReceiver);
+    event RFQByPlatformFilled();
+
+    function init(
+        address owner, 
+        address tokenReceiver,
+        address dodoApprove,
+        address dodoApproveProxy
+    ) external {
+        initOwner(owner);
+        _TOKEN_RECEIVER_ = tokenReceiver;
+        _DODO_APPROVE_ = dodoApprove;
+        _DODO_APPROVE_PROXY_ = dodoApproveProxy;
+    }
+
+
+    function matchingRFQByPlatform(
+        RfqOrder memory order,
+        bytes memory makerSignature,
+        bytes memory takerSignature,
+        uint256 thresholdAddMakerTokenAmount,
+        uint256 makerTokenFeeAmount,
+        address taker,
+        address dodoRouteProxy,
+        bytes memory dodoApiData
+    ) public {
+        require(isAdminListed[msg.sender], "ACCESS_DENIED");
+        require(order.expiration > block.timestamp, "DLOP: EXPIRE_ORDER");
+
+        bytes32 orderHashForMaker = _rfqOrderHash(order);
+        require(ECDSA.recover(orderHashForMaker, makerSignature) == order.maker, "DLOP:INVALID_MAKER_SIGNATURE");
+        require(order.taker == address(0), "DLOP:TAKER_INVALID");
+
+        order.taker = taker;
+        order.makerTokenFeeAmount = makerTokenFeeAmount;
+
+        bytes32 orderHashForTaker = _rfqOrderHash(order);
+        require(ECDSA.recover(orderHashForTaker, takerSignature) == order.taker, "DLOP:INVALID_TAKER_SIGNATURE");
+
+        _settleRFQ(order, thresholdAddMakerTokenAmount, dodoRouteProxy, dodoApiData);
+
+        emit RFQByPlatformFilled();
+    }
+
+    function _settleRFQ(
+        RfqOrder memory order, 
+        uint256 thresholdAddMakerTokenAmount,
+        address dodoRouteProxy,
+        bytes memory dodoApiData
+    ) internal {
+        address taker = order.taker;
+        address maker = order.maker;
+        uint256 takerFillAmount = order.takerAmount;
+        uint256 makerTokenFeeAmount = order.makerTokenFeeAmount;
+        address orderMakerToken = order.makerToken;
+        address orderTakerToken = order.takerToken;
+        uint256 orderMakerAmount = order.makerAmount;
+        
+        IDODOApproveProxy(_DODO_APPROVE_PROXY_).claimTokens(orderTakerToken, taker, address(this), takerFillAmount);
+
+        uint256 originMakerBalance = IERC20(orderMakerToken).balanceOf(address(this));
+        _approveMax(IERC20(orderTakerToken), _DODO_APPROVE_, takerFillAmount);
+        (bool success, ) = dodoRouteProxy.call(dodoApiData);
+        require(success, "API_SWAP_FAILED");
+        uint256 makerBalance = IERC20(orderMakerToken).balanceOf(address(this));
+        uint256 returnMakerAmount = makerBalance.sub(originMakerBalance);
+
+        if(returnMakerAmount >= orderMakerAmount) {
+            uint leftMakerTokenAmount = returnMakerAmount.sub(orderMakerAmount.sub(makerTokenFeeAmount));
+            IERC20(orderMakerToken).safeTransfer(_TOKEN_RECEIVER_, leftMakerTokenAmount);
+        } else {
+            uint addMakerTokenAmount = orderMakerAmount.sub(returnMakerAmount);
+            require(addMakerTokenAmount < thresholdAddMakerTokenAmount, "DLOP: NEED_MORE_MAKERTOKEN");
+            IDODOApproveProxy(_DODO_APPROVE_PROXY_).claimTokens(orderMakerToken, maker, address(this), addMakerTokenAmount);
+            if(makerTokenFeeAmount > 0) {
+                IERC20(orderMakerToken).safeTransfer(_TOKEN_RECEIVER_, makerTokenFeeAmount);
+            }
+        }
+
+        IERC20(orderMakerToken).safeTransfer(taker, orderMakerAmount.sub(makerTokenFeeAmount));
+    }
+
+
+    //============  Ownable ============
+    function addAdminList (address userAddr) external onlyOwner {
+        isAdminListed[userAddr] = true;
+        emit addAdmin(userAddr);
+    }
+
+    function removeAdminList (address userAddr) external onlyOwner {
+        isAdminListed[userAddr] = false;
+        emit removeAdmin(userAddr);
+    }
+
+    function changeTokenReceiver(address newTokenReceiver) external onlyOwner {
+        _TOKEN_RECEIVER_ = newTokenReceiver;
+        emit changeReceiver(newTokenReceiver);
+    }
+
+
+    //============  internal ============
+    function _approveMax(IERC20 token,address to,uint256 amount) internal {
+        uint256 allowance = token.allowance(address(this), to);
+        if (allowance < amount) {
+            if (allowance > 0) {
+                token.safeApprove(to, 0);
+            }
+            token.safeApprove(to, type(uint256).max);
+        }
+    }
+
+    function _rfqOrderHash(RfqOrder memory order) private view returns(bytes32) {
+        return _hashTypedDataV4(
+            keccak256(
+                abi.encode(
+                    RFQ_ORDER_TYPEHASH,
+                    order.makerToken,
+                    order.takerToken,
+                    order.makerAmount,
+                    order.takerAmount,
+                    order.makerTokenFeeAmount,
+                    order.takerFillAmount,
+                    order.maker,
+                    order.taker,
+                    order.expiration,
+                    order.slot
+                )
+            )
+        );
+    }
+ }
--- script/createOrder.js
+++ script/createOrder.js
@@ -1,24 +1,28 @@
 var eth_sig_util_1 = require("eth-sig-util");
 const { parseFixed } = require('@ethersproject/bignumber')
-var privKey = process.env.privKey;
 
 var dodoLimitOrder = "0x969782CEdA52E3a8991C8f91Add31406A781580F"
 var dodoLimitOrderBot = "0xD17bfB2C31021ED1D3ae2C34D04F3f3A69e5dc82"
 
 var Rinkeby_LOABC = "0x0f423838d2e4ce4314d30f8ad42dc41cecde06c5"
 var Rinkeby_LODEF = "0xce7afae63edd2719cd9f46f8afa80eb490ede6a8"
+var privKeyInput = process.env.privKey;
+var privKey = Buffer.from(privKeyInput, 'hex');
 
-var limitOrder = {
-    makerToken: Rinkeby_LOABC,
-    takerToken: Rinkeby_LODEF,
-    makerAmount: parseFixed('10', 18).toString(),
-    takerAmount: parseFixed('1', 18).toString(),
-    takerTokenFeeAmount: '0',
-    maker: '0xbC7814de9e42945C9fFd89D2BFff1a45e07Bdb10',
-    taker: dodoLimitOrderBot,
-    feeRecipient: '0x0000000000000000000000000000000000000000',
-    expiration: Math.floor(new Date().getTime() / 1000 + 60 * 60 * 24 * 10),
-    salt: Math.round(Math.random() * Date.now()) + ''
+
+
+
+var rfqOrder = {
+    makerToken: "0x969782CEdA52E3a8991C8f91Add31406A781580F",//用户买的币地址
+    takerToken: "0xce7afae63edd2719cd9f46f8afa80eb490ede6a8",//用户卖的币地址
+    makerAmount: parseFixed('10', 18).toString(), //做市商的报价
+    takerAmount: parseFixed('1', 18).toString(), //用户的输入
+    makerTokenFeeAmount: '0', //默认0
+    takerFillAmount: parseFixed('1', 18).toString(),//等于takerAmount
+    maker: '0xbC7814de9e42945C9fFd89D2BFff1a45e07Bdb10', //做市商地址
+    taker: '0x0000000000000000000000000000000000000000',//默认空地址
+    expiration: Math.floor(new Date().getTime() / 1000 + 60 * 60 * 24 * 10), //做市商订单的过期时间，时间戳 秒
+    slot: Math.round(Math.random() * Date.now()) + '' //做市商维护的唯一订单标识
 }
 
 var EIP712_DOMAIN = [
@@ -28,20 +32,86 @@ var EIP712_DOMAIN = [
     { name: 'verifyingContract', type: 'address' },
 ];
 
+var RFQ_ORDER_STRUCTRUE = [
+    { name: 'makerToken', type: 'address' },
+    { name: 'takerToken', type: 'address' },
+    { name: 'makerAmount', type: 'uint256' },
+    { name: 'takerAmount', type: 'uint256' },
+    { name: 'makerTokenFeeAmount', type: 'uint256' },
+    { name: 'takerFillAmount', type: 'uint256' },
+    { name: 'maker', type: 'address' },
+    { name: 'taker', type: 'address' },
+    { name: 'expiration', type: 'uint256' },
+    { name: 'slot', type: 'uint256' }
+];
+
+createRfqOrder();
+
+async function createRfqOrder() {
+    var rfqOrderTypedData = buildRfqOrderTypedData(rfqOrder);
+    var signature = await signTypedData(rfqOrderTypedData);
+
+    console.log("signature:", signature) //做市商的签名
+}
+
+async function signTypedData(typedData) {
+    const result = await eth_sig_util_1.signTypedData_v4(privKey, {
+        data: typedData,
+    });
+
+
+    const recovered = eth_sig_util_1.recoverTypedSignature_v4({
+        data: typedData,
+        sig: result,
+    });
+
+    console.log("recovered:", recovered)  //得到做市商公钥地址，进行比对
+
+    return result
+}
+
+
+function buildRfqOrderTypedData(rfqOrder) {
+    return {
+        primaryType: 'Order',
+        types: {
+            EIP712Domain: EIP712_DOMAIN,
+            RfqOrder: RFQ_ORDER_STRUCTRUE,
+        },
+        domain: {
+            name: 'DODO Limit Order Protocol',
+            version: '1',
+            chainId: 4,
+            verifyingContract: dodoLimitOrder,
+        },
+        message: rfqOrder,
+    };
+}
+
+
+// createOrder();
+
+var limitOrder = {
+    makerToken: Rinkeby_LOABC,
+    takerToken: Rinkeby_LODEF,
+    makerAmount: parseFixed('10', 18).toString(),
+    takerAmount: parseFixed('1', 18).toString(),
+    maker: '0xbC7814de9e42945C9fFd89D2BFff1a45e07Bdb10',
+    taker: dodoLimitOrderBot,
+    expiration: Math.floor(new Date().getTime() / 1000 + 60 * 60 * 24 * 10),
+    salt: Math.round(Math.random() * Date.now()) + ''
+}
+
 var LIMIT_ORDER_STRUCTRUE = [
     { name: 'makerToken', type: 'address' },
     { name: 'takerToken', type: 'address' },
     { name: 'makerAmount', type: 'uint256' },
     { name: 'takerAmount', type: 'uint256' },
-    { name: 'takerTokenFeeAmount', type: 'uint256' },
     { name: 'maker', type: 'address' },
     { name: 'taker', type: 'address' },
-    { name: 'feeRecipient', type: 'address' },
     { name: 'expiration', type: 'uint256' },
     { name: 'salt', type: 'uint256' }
 ];
-
-createOrder();
 
 async function createOrder() {
     var limitOrderTypedData = buildLimitOrderTypedData(limitOrder);
@@ -52,13 +122,6 @@ async function createOrder() {
     console.log("salt:", limitOrder.salt)
 }
 
-
-async function signTypedData(typedData) {
-    const result = await eth_sig_util_1.signTypedData_v4(Buffer.from(privKey, 'hex'), {
-        data: typedData,
-    });
-    return result
-}
 
 function buildLimitOrderTypedData(limitOrder) {
     return {
