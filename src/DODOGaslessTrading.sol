1fd91f49f6995df5a0cbc46d8267604e72c6ff6c
radar bear <leimingda94@gmail.com>
2022-05-15 15:19:10 +0800

    refactor DODO Gasless trading

---
+++ src/DODOGaslessTrading.sol
@@ -0,0 +1,211 @@
+/*
+    Copyright 2021 DODO ZOO.
+    SPDX-License-Identifier: Apache-2.0
+*/
+
+pragma solidity 0.8.4;
+
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import {IDODOApproveProxy} from "./intf/IDODOApproveProxy.sol";
+import {IERC20} from "./intf/IERC20.sol";
+import {SafeERC20} from "./lib/SafeERC20.sol";
+import {EIP712} from "../node_modules/@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
+import {ECDSA} from "../node_modules/@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+
+/**
+ * @title DODOGaslessTrading
+ * @author DODO Breeder
+ */
+
+contract DODOGaslessTrading is
+    EIP712("DODO Limit Order Protocol", "1"),
+    Ownable
+{
+    using SafeERC20 for IERC20;
+
+    bytes32 public constant GASLESS_ORDER_TYPEHASH =
+        keccak256(
+            "Order(address signer,address fromToken,address toToken,uint256 fromAmount,uint256 toAmount,uint256 expiration,uint256 slot)"
+        );
+
+    struct GaslessOrder {
+        address signer;
+        address fromToken;
+        address toToken;
+        uint256 fromAmount;
+        uint256 toAmount;
+        uint256 expiration;
+        uint256 slot;
+    }
+
+    //=============== Storage ===============
+
+    address public _INSURANCE_;
+    address public immutable _DODO_APPROVE_;
+    address public immutable _DODO_APPROVE_PROXY_;
+    address public immutable _DODO_ROUTE_PROXY_;
+    mapping(address => bool) public _IS_ADMIN_;
+
+    //=============== Events ===============
+
+    event AddAdmin(address admin);
+
+    event RemoveAdmin(address admin);
+
+    event ChangeInsurance(address newInsurance);
+
+    event GaslessOrderFilled(bytes32 orderHash);
+
+    //=============== Functions ===============
+
+    constructor(
+        address owner,
+        address insurance,
+        address dodoApprove,
+        address dodoApproveProxy,
+        address dodoRouteProxy
+    ) {
+        transferOwnership(owner);
+        _INSURANCE_ = insurance;
+        _DODO_APPROVE_ = dodoApprove;
+        _DODO_APPROVE_PROXY_ = dodoApproveProxy;
+        _DODO_ROUTE_PROXY_ = dodoRouteProxy;
+    }
+
+    function matchingRFQByPlatform(
+        GaslessOrder calldata order,
+        bytes calldata traderSignature,
+        bytes calldata dodoRouteData,
+        uint256 maxCompensation
+    ) external {
+        require(_IS_ADMIN_[msg.sender], "ACCESS_DENIED");
+
+        // verify order
+        bytes32 orderHash = _hashOrder(order);
+        require(
+            ECDSA.recover(orderHash, traderSignature) == order.signer,
+            "DLOP:INVALID_SIGNATURE"
+        );
+        require(order.expiration > block.timestamp, "DLOP:ORDER_EXPIRED");
+
+        // flash swap: transfer trader's FROM token in
+        IDODOApproveProxy(_DODO_APPROVE_PROXY_).claimTokens(
+            order.fromToken,
+            order.signer,
+            address(this),
+            order.fromAmount
+        );
+
+        // swap using DODO Route
+        uint256 originToTokenBalance = IERC20(order.toToken).balanceOf(
+            address(this)
+        );
+        _approveMax(IERC20(order.fromToken), _DODO_APPROVE_, order.fromAmount);
+        (bool success, ) = _DODO_ROUTE_PROXY_.call(dodoRouteData);
+        require(success, "DLOP:DODO_ROUTE_FAILED");
+        uint256 toTokenBalance = IERC20(order.toToken).balanceOf(address(this));
+        uint256 swapResult = toTokenBalance - originToTokenBalance;
+
+        // pay trader TO token
+        if (swapResult > order.toAmount) {
+            // fund extra TO token to insurance
+            IERC20(order.toToken).safeTransfer(order.signer, order.toAmount);
+            IERC20(order.toToken).safeTransfer(
+                _INSURANCE_,
+                IERC20(order.toToken).balanceOf(address(this))
+            );
+        } else {
+            IERC20(order.toToken).safeTransfer(order.signer, swapResult);
+            // compensate the trader
+            uint256 compensation = order.toAmount - swapResult;
+            require(
+                compensation <= maxCompensation,
+                "DLOP:COMPENSATION_EXCEED"
+            );
+            if (IERC20(order.toToken).balanceOf(_INSURANCE_) > compensation) {
+                // compensate TO token if balance enough
+                IDODOApproveProxy(_DODO_APPROVE_PROXY_).claimTokens(
+                    order.toToken,
+                    _INSURANCE_,
+                    order.signer,
+                    compensation
+                );
+            } else {
+                // otherwise compensate FROM token with the price signed in order
+                IDODOApproveProxy(_DODO_APPROVE_PROXY_).claimTokens(
+                    order.fromToken,
+                    _INSURANCE_,
+                    order.signer,
+                    (compensation * order.fromAmount) / order.toAmount
+                );
+            }
+        }
+
+        emit GaslessOrderFilled(orderHash);
+    }
+
+    //============ Ownable ============
+
+    function addAdmin(address userAddr) external onlyOwner {
+        _IS_ADMIN_[userAddr] = true;
+        emit AddAdmin(userAddr);
+    }
+
+    function removeAdmin(address userAddr) external onlyOwner {
+        _IS_ADMIN_[userAddr] = false;
+        emit RemoveAdmin(userAddr);
+    }
+
+    function changeInsurance(address newInsurance) external onlyOwner {
+        _INSURANCE_ = newInsurance;
+        emit ChangeInsurance(newInsurance);
+    }
+
+    //============ Private ============
+
+    function _approveMax(
+        IERC20 token,
+        address to,
+        uint256 amount
+    ) private {
+        uint256 allowance = token.allowance(address(this), to);
+        if (allowance < amount) {
+            if (allowance > 0) {
+                token.safeApprove(to, 0);
+            }
+            token.safeApprove(to, type(uint256).max);
+        }
+    }
+
+    function _hashOrder(GaslessOrder memory order)
+        private
+        view
+        returns (bytes32)
+    {
+        // keccak256(
+        //     abi.encode(
+        //         RFQ_ORDER_TYPEHASH,
+        //         order.signer,
+        //         order.fromToken,
+        //         order.toToken,
+        //         order.fromAmount,
+        //         order.toAmount
+        //         order.expiration,
+        //         order.slot
+        //     )
+        // )
+        bytes32 structHash;
+        bytes32 orderTypeHash = GASLESS_ORDER_TYPEHASH;
+        assembly {
+            let start := sub(order, 32)
+            let tmp := mload(start)
+            // 256 = (1+7)*32
+            // [0...32)   bytes: EIP712_ORDER_TYPE
+            // [32...256) bytes: order
+            mstore(start, orderTypeHash)
+            structHash := keccak256(start, 256)
+            mstore(start, tmp)
+        }
+        return _hashTypedDataV4(structHash);
+    }
+}
